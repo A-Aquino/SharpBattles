e71fc23bb6b5bfe1403dbf4255a468c5da3a657c
Alexander Aquino <A-Aquino@users.noreply.github.com>
2017-08-17 02:05:48 -0400

    Initial commit

---
+++ SharpBattles/Battle.xaml.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SharpBattles
+{
+    /// <summary>
+    /// Interaction logic for Battle.xaml
+    /// </summary>
+    public partial class Battle : Page
+    {
+
+        string p1;
+        string p2;
+        string[] p1Attacks;
+        string[] p2Attacks;
+        int p1Health;
+        int p2Health;
+        int p1Defend;
+        int p2Defend;
+
+        Random random;
+
+        public Battle(string a, string b)
+        {
+            InitializeComponent();
+            p1 = a;
+            p2 = b;
+            p1Attacks = getAttacks(p1);
+            p2Attacks = getAttacks(p2);
+            p1Health = 1000;
+            p2Health = 1000;
+            p1Defend = 0;
+            p2Defend = 0;
+
+            random = new Random();
+
+            //Prepare layout
+            _vsText.Text = p1 + " vs. " + p2;
+            _player1.Source = new BitmapImage(new Uri(@"Images/" + p1 + ".png", UriKind.Relative));
+            _player2.Source = new BitmapImage(new Uri(@"Images/" + p2 + ".png", UriKind.Relative));
+            _player1Name.Text = p1;
+            _player2Name.Text = p2;
+            _battleNotes.Text = "A wild " + p2 + " appears!";
+
+            //Make attacks
+            makeAttacks();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new Attacks().Show();
+        }
+
+        //Attack List
+        private string[] getAttacks(string player)
+        {
+            switch (player)
+            {
+                //Player Classes
+                case "Swordsman": return new string[] { "Slash", "Stab", "Dodge" };
+                case "Archer": return new string[] { "Shoot", "Defend", "Dodge" };
+                case "Mage": return new string[] { "Fireball", "Thunder", "Heal" };
+                case "Knight": return new string[] { "Stab", "Defend" };
+
+                //Enemies
+                case "Slime": return new string[] { "Slash", "Dodge", "Heal" };
+                case "Goblin": return new string[] { "Slash", "Shoot", "Defend" };
+                case "Bat": return new string[] { "Slash", "Dodge" };
+                case "Mimic": return new string[] { "Fireball", "Thunder", "Dark Arts" };
+                case "Witch": return new string[] { "Thunder", "Dark Arts", "Heal" };
+                case "Dragon": return new string[] { "Stab", "Thunder", "Defend" };
+                case "Wyvern": return new string[] { "Stab", "Dark Arts", "Defend" };
+                default: return null;
+            }
+        }
+
+        //Make Attack Buttons
+        private void makeAttacks()
+        {
+            foreach (string s in p1Attacks)
+                makeButton(s);
+        }
+
+        //Update Player Health
+        private string updateP1Health(int v)
+        {
+            string t = "";
+            if (v < 0)
+            {
+                if (p1Defend == 1) { v = Convert.ToInt32(Math.Floor(Convert.ToDouble(v) / 4)); }
+                if (p1Defend == 2) { if (random.Next(0, 100) > 50) v = 0; }
+                t = "Dealt " + Math.Abs(v) + " damage.";
+                if (p1Defend == 1) t += " " + p1 + " defended the attack!";
+                if (p1Defend == 2 && v != 0) t += " " + p1 + " attempted to dodge, but failed!";
+                if (p1Defend == 2 && v == 0) t += " " + p1 + " successfully dodged the attack!";
+                p1Defend = 0;
+            }
+            p1Health += v;
+            if (p1Health < 0) p1Health = 0;
+            if (p1Health > 1000) p1Health = 1000;
+            _player1Health.Text = "" + p1Health;
+            return t;
+        }
+
+        //Update Enemy Health
+        private string updateP2Health(int v)
+        {
+            string t = "";
+            if (v < 0)
+            {
+                if (p2Defend == 1) { v = Convert.ToInt32(Math.Floor(Convert.ToDouble(v) / 4)); }
+                if (p2Defend == 2) { if (random.Next(0, 100) > 50) v = 0; }
+                t = "Dealt " + Math.Abs(v) + " damage.";
+                if (p2Defend == 1) t += " " + p2 + " defended the attack!";
+                if (p2Defend == 2 && v != 0) t += " " + p2 + " attempted to dodge, but failed!";
+                if (p2Defend == 2 && v == 0) t += " " + p2 + " successfully dodged the attack!";
+                p2Defend = 0;
+            }
+            p2Health += v;
+            if (p2Health < 0) p2Health = 0;
+            if (p2Health > 1000) p2Health = 1000;
+            _player2Health.Text = "" + p2Health;
+            return t;
+        }
+
+        //Pick a random enemy move
+        private string pickEnemyMove()
+        {
+            return p2Attacks[random.Next(0, p2Attacks.Length)];
+        }
+
+        //Handle all button presses
+        private void buttonPress(object sender, RoutedEventArgs e)
+        {
+            //Remove Buttons
+            _attackList.Children.Clear();
+
+            //Button Text
+            Button b = (Button)sender;
+            string t = (string)b.Content;
+
+            //Check End State
+	        if(t=="Battle End"||t=="Game Over") {
+		        this.NavigationService.GoBack();
+		        return;
+	        }
+	
+	        //Check Health
+	        if(p2Health==0) {
+		        _battleNotes.Text = p1+" wins the battle!";
+		        makeButton("Battle End");
+		        return;
+	        }
+	        if(p1Health==0) {
+		        _battleNotes.Text = p1+" died!";
+		        makeButton("Game Over");
+		        return;
+	        }
+	
+	        //Continue
+	        if(t=="Continue") {
+		
+		        //Pick enemy moves
+		        t = pickEnemyMove();
+
+		        //Smarter AI
+		        while( (p2Defend>0&&(t=="Dodge"||t=="Defend")) || (p2Health==1000&&t=="Heal") )
+			        t = pickEnemyMove();
+		
+		        //Enemy moves
+		        if(t=="Slash") {
+                    _battleNotes.Text = p2 + " used Slash. " + updateP1Health(-200 + random.Next(0, 20) - 10);
+		        }
+		        if(t=="Stab") {
+                    if (random.Next(0, 100) > 50)
+                        _battleNotes.Text = p2 + " used Stab. " + updateP1Health(-400 + random.Next(0, 30) - 15);
+			        else
+                        _battleNotes.Text = p2 + " used Stab, and missed.";
+		        }
+		        if(t=="Shoot") {
+                    if (random.Next(0, 100) > 25)
+                        _battleNotes.Text = p2 + " used Shoot. " + updateP1Health(-300 + random.Next(0, 30) - 15);
+			        else
+                        _battleNotes.Text = p2 + " used Shoot, and missed.";
+		        }
+		        if(t=="Dodge") {
+                    _battleNotes.Text = p2 + " prepares to dodge an attack.";
+                    p2Defend = 2;
+		        }
+		        if(t=="Defend") {
+                    _battleNotes.Text = p2 + " prepares to defend an attack.";
+                    p2Defend = 1;
+		        }
+		        if(t=="Fireball") {
+                    if (random.Next(0, 100) > 25)
+                        _battleNotes.Text = p2 + " used Fireball. " + updateP1Health(-300 + random.Next(0, 30) - 15);
+			        else
+                        _battleNotes.Text = p2 + " used Fireball, and missed.";
+		        }
+		        if(t=="Thunder") {
+                    if (random.Next(0, 100) > 75)
+                        _battleNotes.Text = p2 + " used Thunder. " + updateP1Health(-500 + random.Next(0, 40) - 20);
+			        else
+                        _battleNotes.Text = p2 + " used Thunder, and missed.";
+		        }
+		        if(t=="Heal") {
+			        int heal = 200+random.Next(0, 150) - 100;
+                    _battleNotes.Text = p2 + " used Heal, and recovered " + heal + " HP!";
+                    updateP2Health(heal);
+		        }
+		        if(t=="Dark Arts") {
+                    if (random.Next(0, 100) > 85)
+                        _battleNotes.Text = p2 + " used Dark Arts. " + updateP1Health(-800 + random.Next(0, 300) - 150);
+			        else
+                        _battleNotes.Text = p2 + " used Dark Arts, and missed.";
+		        }
+                if (p1Health > 0)
+			        makeAttacks();
+		        else
+			        makeButton("Continue");
+	        }
+	
+	        //Player moves
+	        else {
+		        if(t=="Slash") {
+                    _battleNotes.Text = p1 + " used Slash. " + updateP2Health(-200 + random.Next(0, 20) - 10);
+		        }
+		        if(t=="Stab") {
+                    if (random.Next(0, 100) > 50)
+                        _battleNotes.Text = p1 + " used Stab. " + updateP2Health(-400 + random.Next(0, 30) - 15);
+			        else
+                        _battleNotes.Text = p1 + " used Stab, and missed.";
+		        }
+		        if(t=="Shoot") {
+                    if (random.Next(0, 100) > 25)
+                        _battleNotes.Text = p1 + " used Shoot. " + updateP2Health(-300 + random.Next(0, 30) - 15);
+			        else
+                        _battleNotes.Text = p1 + " used Shoot, and missed.";
+		        }
+		        if(t=="Dodge") {
+                    _battleNotes.Text = p1 + " prepares to dodge an attack.";
+                    p1Defend = 2;
+		        }
+		        if(t=="Defend") {
+                    _battleNotes.Text = p1 + " prepares to defend an attack.";
+                    p1Defend = 1;
+		        }
+		        if(t=="Fireball") {
+                    if (random.Next(0, 100) > 25)
+                        _battleNotes.Text = p1 + " used Fireball. " + updateP2Health(-300 + random.Next(0, 30) - 15);
+			        else
+                        _battleNotes.Text = p1 + " used Fireball, and missed.";
+		        }
+		        if(t=="Thunder") {
+                    if (random.Next(0, 100) > 75)
+                        _battleNotes.Text = p1 + " used Thunder. " + updateP2Health(-500 + random.Next(0, 40) - 20);
+			        else
+                        _battleNotes.Text = p1 + " used Thunder, and missed.";
+		        }
+		        if(t=="Heal") {
+                    int heal = 200 + random.Next(0, 150) - 100;
+                    _battleNotes.Text = p1 + " used Heal, and recovered " + heal + " HP!";
+                    updateP1Health(heal);
+		        }
+		        makeButton("Continue");
+        }
+        }
+
+        //Make buttons
+        private void makeButton(string t)
+        {
+            Button b = new Button();
+            b.Height = 32;
+            b.FontSize = 18;
+            b.Click += buttonPress;
+            b.Content = t;
+            _attackList.Children.Add(b);
+        }
+
+
+    }
+}
---
+++ SharpBattles/CharacterSelect.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SharpBattles
+{
+    /// <summary>
+    /// Interaction logic for CharacterSelect.xaml
+    /// </summary>
+    public partial class CharacterSelect : Page
+    {
+        string p1;
+        string p2;
+
+        public CharacterSelect()
+        {
+            InitializeComponent();
+            p1 = "Swordsman";
+            p2 = "Slime";
+        }
+
+        private void _player1Select_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (p1 == null) return;
+            p1 = _player1Select.SelectedItem.ToString();
+            p1 = p1.Substring(38, p1.Length - 38);
+            _vsText.Text = p1 + " vs. " + p2;
+            _player1.Source = new BitmapImage(new Uri(@"Images/" + p1 + ".png", UriKind.Relative));
+        }
+
+        private void _player2Select_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (p2 == null) return;
+            p2 = _player2Select.SelectedItem.ToString();
+            p2 = p2.Substring(38, p2.Length - 38);
+            _vsText.Text = p1 + " vs. " + p2;
+            _player2.Source = new BitmapImage(new Uri(@"Images/" + p2 + ".png", UriKind.Relative));
+        }
+
+        private void _startBattle_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Battle(p1, p2));
+        }
+    }
+}
